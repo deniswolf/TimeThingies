743a27c0baa9e5d03848bf2e710a078253fb6c94
Denis Wolf <dear.mr.dw@gmail.com>
2013-05-23 01:14:00 +0300

    first e2e spec

---
+++ test/e2e/main.js
@@ -0,0 +1,21 @@
+describe('Time Thingies App', function() {
+
+    describe('Main page', function() {
+
+        beforeEach(function() {
+            browser().navigateTo('/');
+        });
+
+        it ('has two links', function () {
+            expect(element('a').count()).toBe(2);
+        });
+
+        it ('contains button to personal Timer', function () {
+            expect(element('a[href*=me]').count()).toBe(1);
+        });
+
+        it ('contains button to friends Timers', function () {
+            expect(element('a[href*=friends]').count()).toBe(1);
+        });
+    });
+});
